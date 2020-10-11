ff2593ac731f7091397739c6ad2714dbe43aea2c
Thilina Pathirage <62783923+Thilina-Pathirage@users.noreply.github.com>
2020-10-12 00:59:10 +0530

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -3,6 +3,17 @@ print ("Hello, Python")
 print ("This is a sample Python file for Python basics.")
 print ("Here we discuss basic input and print methods.")
 print () # Insert piece 1 below
+b="Given below is turtle command."
+print (b)
+qazi_turtle=turtle.Turtle()
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+print () # insert piece 2 below
 var = 1
  print (var)
  var2 = 45
