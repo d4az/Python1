49b94680a1a485526ab9b1abf860d9fb506ffe90
AvishkaSooriyapperuma <[avishkadilshan400@gmail.com]>
2020-10-12 00:07:08 +0530

    From Avishka

--- Sample.py
+++ Sample.py
@@ -3,3 +3,10 @@ print ("Hello, Python")
 print ("This is a sample Python file for Python basics.")
 print ("Here we discuss basic input and print methods.")
 print () # Insert piece 1 below
+var = 1
+ print (var)
+ var2 = 45
+ print (var2)
+ n = input ("Insert your number below: ")
+ print (n)
+ print ("Sample code ends here.")
