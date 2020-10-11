34a7a4b9a12ad04f6537cb8b2799ba98da3ddfb8
SurajiEkanayaka <64369018+SurajiEkanayaka@users.noreply.github.com>
2020-10-12 00:17:58 +0530

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -3,6 +3,12 @@ print ("Hello, Python")
 print ("This is a sample Python file for Python basics.")
 print ("Here we discuss basic input and print methods.")
 print () # Insert piece 1 below
+ print ("This is a command to display what is stated inside paranthesis")
+ print (5)
+ print (67)
+ print ("This is Python")
+ print ("This is the world's most popular programming language")
+ print () #insert piece 3 below
 var = 1
  print (var)
  var2 = 45
