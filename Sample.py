468d5fce49798b2afbc89ce2ed379e55db1402cc
NishuDissanayake <63721945+NishuDissanayake@users.noreply.github.com>
2020-10-11 23:10:25 +0530

    Create Sample.py
    Sample Python file

---
+++ Sample.py
@@ -0,0 +1,5 @@
+import turtle
+print ("Hello, Python")
+print ("This is a sample Python file for Python basics.")
+print ("Here we discuss basic input and print methods.")
+print () # Insert piece 1 below
